2330b685365dce84c0f615452b090a3f12338862
Clément Toquet <toquet.clement@gmail.com>
2023-06-15 10:59:25 +0200

    put popular rooms in foreach, issue with css

--- home.php
+++ home.php
@@ -32,6 +32,36 @@
         <section id="popular-rooms" class="block__section">
             <h2 class="block__section__title">Top 3</h2>
             <ul class="block__section__list">
+
+
+                <?php
+                foreach ($popularRooms as $index => $room) {
+                    $numberRoom = $index +1;
+                    echo 
+                    "<li class='block__section__list__itm'>"
+                        ."<article class='block__section__list__itm--wrapper'>"
+                            ."<h4 class='block__section__list__itm__title'>$numberRoom</h4>"
+                            ."<div class='tile'>
+                                <a href='#' class='tile__left'>
+                                    <img class='tile__img' src='img/Icone Player.svg' alt=''>
+                                </a>
+                                <div class='tile__right'>
+                                    <div class='tile__right--layer'>    
+                                    </div>
+                                    <img src='img/cup.svg' alt=''>"
+                                    ."<div class='tile__right'>"
+                                        ."<p>".$room["nameBestUser"]."</p>"
+                                        ."<p>".$room["scoreBestUser"]." pts"."</p>"
+                                    ."</div>"
+                                ."</div>"
+                            ."</div>"
+                        ."</article>"
+                        ."<p class='tile__label'>".$room['nameRoom']."</p>"
+                    ."</li>";};                
+                ?>
+
+
+
                 <li class="block__section__list__itm">
                     <!--article ?-->
                     <article class="block__section__list__itm--wrapper">
@@ -50,7 +80,7 @@
                                     ?>
                                     <!-- <p>User_1425</p> -->
                                     <?php
-                                        echo "<p>".$popularRooms[0]["scoreBestUser"]." pts"."</php>"
+                                        echo "<p>".$popularRooms[0]["scoreBestUser"]." pts"."</p>"
                                     ?>
                                     <!-- <p>1000pts</p> -->
                                 </div>
