95d0c84cc98c5d3efa7e09204c088c528b5698c7
Clément Toquet <toquet.clement@gmail.com>
2023-07-08 22:10:52 +0200

    rewriting API call to avoid nested if

--- api.php
+++ api.php
@@ -43,40 +43,75 @@ try {
     // see why exception never trigger
 }
 
-// check if call to API is made
-if(isset($playlistJSON["tracks"])) {
-    $playlistTitle = $dataRoom["name_room"];
-    $JSON ["playlistName"] = $playlistTitle;
-    $listOfTracks = $playlistJSON["tracks"]["data"];
+// // check if call to API is made
+// if(isset($playlistJSON["tracks"])) {
+//     $playlistTitle = $dataRoom["name_room"];
+//     $JSON ["playlistName"] = $playlistTitle;
+//     $listOfTracks = $playlistJSON["tracks"]["data"];
 
-    // check if playlist is > 40
-    $listOfTracksWithPreview = checkIfTrackHasPreview($listOfTracks);
-    if(count($listOfTracksWithPreview) >= 40) {
-        // set $isOk to true because whith this datas we can play
-        $isOk = true;
-        foreach($listOfTracksWithPreview as $track) {
-            $tracks [] = [
-                "artist" =>  $track["artist"]["name"],
-                "track" => $track['title_short'],
-                "preview" => $track["preview"]
-            ];
-        }
-    } else {
-        echo json_encode([
-            'result' => $isOk,
-            'msg' => "Pas assez de tracks dans cette playlist"
-            ]);
-        exit;
-    } 
-} else {
+//     // check if playlist is > 40
+//     $listOfTracksWithPreview = checkIfTrackHasPreview($listOfTracks);
+//     if(count($listOfTracksWithPreview) >= 40) {
+//         // set $isOk to true because whith this datas we can play
+//         $isOk = true;
+//         foreach($listOfTracksWithPreview as $track) {
+//             $tracks [] = [
+//                 "artist" =>  $track["artist"]["name"],
+//                 "track" => $track['title_short'],
+//                 "preview" => $track["preview"]
+//             ];
+//         }
+//     } else {
+//         echo json_encode([
+//             'result' => $isOk,
+//             'msg' => "Pas assez de tracks dans cette playlist",
+//             'playlistLength' => count($listOfTracksWithPreview)
+//             ]);
+//         exit;
+//     } 
+// } else {
+//     echo json_encode([
+//         'result' => $isOk,
+//         'msg' => "Pas de playlist à cet ID"
+//         ]);
+//     exit;
+// }
+
+
+
+// check if call to API is made
+if(!isset($playlistJSON["tracks"])) {
     echo json_encode([
         'result' => $isOk,
         'msg' => "Pas de playlist à cet ID"
         ]);
     exit;
 }
+$playlistTitle = $dataRoom["name_room"];
+$JSON ["playlistName"] = $playlistTitle;
+$listOfTracks = $playlistJSON["tracks"]["data"];
+// check if playlist is > 40
+$listOfTracksWithPreview = checkIfTrackHasPreview($listOfTracks);
 
+if(!count($listOfTracksWithPreview) >= 40) {
+    echo json_encode([
+        'result' => $isOk,
+        'msg' => "Pas assez de tracks dans cette playlist",
+        'playlistLength' => count($listOfTracksWithPreview)
+    ]);
+    exit;
+}
 
+// set $isOk to true because whith this datas we can play
+$isOk = true;
+foreach($listOfTracksWithPreview as $track) {
+    $tracks [] = [
+        "artist" =>  $track["artist"]["name"],
+        "track" => $track['title_short'],
+        "preview" => $track["preview"]
+    ];
+}
+    
 // faire une structure de JSON pour mettre dans le localStorage et que ce soit récupérer par mon script de jeu JS
 
 $JSON ["tracks"] = $tracks;
